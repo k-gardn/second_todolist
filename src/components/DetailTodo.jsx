6572ab382a675e1d605fb293e9e6b7544092a99c
k-gardn <rladnjs15@naver.com>
2022-08-24 10:27:44 +0900

    기능구현 완료

--- src/components/DetailTodo.jsx
+++ src/components/DetailTodo.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 // import { useDispatch/**, useSelector**/ } from "react-redux";
 // import { addTodo } from "../redux/modules/todos";
-// import styled from "styled-components";
+import styled from "styled-components";
 
 const DetailTodo = () => {
 
@@ -18,12 +18,25 @@ const DetailTodo = () => {
     const content = location.state.content;
 
     return(
-        <div>
-            <label>ID : {id} </label>
+        <StDetail>
+            <div>
+            <div>ID : {id}</div>
             <button onClick={() => {navigate("/")}}>이전으로</button>
             <h2 >{title}</h2> 
-            <p >{content}</p>
-        </div>
+            <p>{content}</p>
+        
+            </div>
+                
+            
+        </StDetail>
     );
 };
 export default DetailTodo;
+
+const StDetail = styled.div`
+  display: flex;
+  gap: 12px;
+  flex-wrap: wrap;
+  /* text-align: center; */
+`; 
+
